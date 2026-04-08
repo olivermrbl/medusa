f7354283db18cc61963bf6f992f9e737ac54e4c0
Oli Juhl <59018053+olivermrbl@users.noreply.github.com>
2026-04-08 09:33:36 +0200

    Update view-configurations.ts

--- packages/medusa/src/feature-flags/view-configurations.ts
+++ packages/medusa/src/feature-flags/view-configurations.ts
@@ -7,4 +7,6 @@ const ViewConfigurationsFeatureFlag: FlagSettings = {
   description: "[WIP] Enable view configurations for data tables",
 }
 
+console.log("testing this")
+
 export default ViewConfigurationsFeatureFlag
